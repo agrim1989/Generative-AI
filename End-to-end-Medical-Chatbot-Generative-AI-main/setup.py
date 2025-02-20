f74ae556a1f08071de0240c54d8671fa18a30b15
Agrim <agrim89@gmail.com>
2025-02-20 11:41:10 +0530

    New Structur

---
+++ End-to-end-Medical-Chatbot-Generative-AI-main/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Agrim Sharma',
+    author_email= 'agrim89@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
